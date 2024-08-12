88de462e9e53e19656f66e2b670eefa84d5cdf9c
UnABC <75528021+UnABC@users.noreply.github.com>
2024-08-12 22:46:53 +0900

    てすと

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,7 +1,21 @@
-﻿#include <Siv3D.hpp>
+﻿#include "common.hpp"
+#include "Game.hpp"
 
 void Main() {
+	// 背景の色を設定する | Set the background color
+	Scene::SetBackground(ColorF{ 0.0, 0.0, 0.0 });
+	//windowsサイズ
+	Window::Resize(1920, 1080);
+	//フルスクリーン
+	Window::SetFullscreen(true);
+	//タイトル
+	Window::SetTitle(U"ShoutWars");
 
-	while (System::Update()) {
-	}
+	App manager;
+	//manager.add<Title>(State::Title);
+	//manager.add<Matching>(State::Matching);
+	manager.add<Game>(State::Game);
+	//manager.add<Result>(State::Result);
+
+	while (System::Update() && manager.update()) {};
 }
