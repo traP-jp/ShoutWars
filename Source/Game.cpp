0e179e16c5cf11f5fc70cb3195ef018bb6773f10
UnABC <75528021+UnABC@users.noreply.github.com>
2024-08-14 01:58:28 +0900

    キャラの移動、キャラ同士の値判定まで

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -1,13 +1,19 @@
 ﻿#include "Game.hpp"
 
 #define player_number 0
+#define another_player_number 1
+
+using namespace std;
 
 Game::Game(const InitData& init) : IScene(init),
-player_img(player_sum), player_flag(player_sum,true)
+player_img(player_sum),
+player_flag(player_sum, true)
 {
-	player_img.at(0).push_back(Texture{Unicode::Widen("../images/player1.png")});
-	player_img.at(1).push_back(Texture{ Unicode::Widen("../images/player2.png")});
+	player_img.at(0).push_back(Texture{ Unicode::Widen("../images/player1.png") });
+	player_img.at(1).push_back(Texture{ Unicode::Widen("../images/player2.png") });
 
+	player_pos[0][0] = { 100.0,player_min_y };
+	player_pos[1][0] = { 1200,player_min_y };
 	internal_timer = Time::GetMillisec();
 }
 
@@ -18,10 +24,10 @@ int Game::getkey() {
 	retkey |= 4 * KeyS.pressed();
 	retkey |= 8 * KeyD.pressed();
 
-	retkey |= 1*KeyUp.pressed();
-	retkey |= 2*KeyLeft.pressed();
-	retkey |= 4*KeyDown.pressed();
-	retkey |= 8*KeyRight.pressed();
+	retkey |= 1 * KeyUp.pressed();
+	retkey |= 2 * KeyLeft.pressed();
+	retkey |= 4 * KeyDown.pressed();
+	retkey |= 8 * KeyRight.pressed();
 	return retkey;
 }
 
@@ -31,25 +37,41 @@ void Game::update() {
 }
 
 void Game::update_player() {
+	int now_time = Time::GetMillisec();
 	//プレイヤー(ユーザー操作)の移動処理////////////////////////////////////////////////////////////
-	if (int gotkey = getkey()) {
-		//TODO:左右の移動も時間で管理する!!
-		if (gotkey&2) player_pos[player_number] += go_left * player_speed[player_number];
-		if (gotkey&8) player_pos[player_number] += go_right * player_speed[player_number];
-		if ((gotkey&1)&&(player_status[player_number] != 3)) {
-			player_status[player_number] = 3;
-			player_timer[player_number] = Time::GetMillisec();
-		}
-		player_pos[player_number].x = Clamp(player_pos[player_number].x, 50.0, 1850.0);
-	}
-
+	//左右移動処理
 	if (player_status[player_number] & 3) {
-		if (Time::GetMillisec() - player_timer[player_number] < 500) {
-			int t = Time::GetMillisec() - player_timer[player_number];
-			player_pos[player_number].y = player_min_y - 2.0 * t + 0.004 * t * t;
+		int t = now_time - player_timer[player_number][0];
+		if (t < 50) {
+			player_pos[player_number][0].x = player_pos[player_number][1].x + ((player_status[player_number] & 1) ? -1.0 : 1.0) * player_speed[player_number] * t / 50;
 		}else {
-			player_status[player_number] ^= 3;
-			player_pos[player_number].y = player_min_y;
+			player_status[player_number] ^= (player_status[player_number]&1)?1:2;
+		}
+	}
+	//ジャンプ処理
+	if (player_status[player_number] & 4) {
+		int t = now_time - player_timer[player_number][2];
+		if (t < 500) {
+			player_pos[player_number][0].y = player_min_y - 2.0 * t + 0.004 * t * t;
+		}
+		else {
+			player_status[player_number] ^= 4;
+			player_pos[player_number][0].y = player_min_y;
+		}
+	}
+	//キー入力処理
+	if (int gotkey = getkey()) {
+		if ((player_status[player_number] & 3) == 0){
+			if (gotkey & 10) {
+				player_status[player_number] |= (gotkey & 2)?1:2;
+				player_timer[player_number][0] = now_time;
+				player_pos[player_number][1].x = player_pos[player_number][0].x;
+			}
+		}
+		if ((gotkey & 1) && ((player_status[player_number] & 4) == 0)) {
+			player_status[player_number] |= 4;
+			player_timer[player_number][2] = now_time;
+			player_pos[player_number][1].y = player_pos[player_number][0].y;
 		}
 	}
 	//プレイヤー(相手)の移動処理////////////////////////////////////////////////////////////////////
@@ -57,13 +79,32 @@ void Game::update_player() {
 	//TODO：いい感じに実装して
 
 	//プレイヤー同士の相互作用/////////////////////////////////////////////////////////////////////
+	for (int i = 0; i < player_sum; i++) {
+		if (i == player_number) continue;
+		if ((abs(player_pos[player_number][0].x - player_pos[i][0].x) < 40.0)&&(abs(player_pos[player_number][0].y - player_pos[i][0].y) < 242.0)) {
+			if (player_status[i]&3) {
+				//TODO
+			}else {
+				player_pos[i][0].x += (player_pos[player_number][0].x < player_pos[i][0].x) ? 16.0 : -16.0;
+			}
+		}
+	}
 
-	//TODO:明日起きたらここをかけ
+	//プレイヤーの向き
+	if (player_pos[player_number][0].x < player_pos[another_player_number][0].x) {
+		player_direction[player_number] = false;
+		player_direction[another_player_number] = true;
+	}else {
+		player_direction[player_number] = true;
+		player_direction[another_player_number] = false;
+	}
 
 	//技とかの処理/////////////////////////////////////////////////////////////////////////////////
 
 	//TODO:画像来たらやれ
 
+	//移動範囲制限
+	player_pos[player_number][0].x = Clamp(player_pos[player_number][0].x, 50.0, 1850.0);
 }
 
 void Game::drawFadeIn(double t) const {
@@ -79,6 +120,6 @@ void Game::draw() const {
 void Game::draw_player() const {
 	for (int i = 0; i < player_sum;i++) {
 		if (!player_flag[i]) continue;
-		player_img.at(i).at(0).drawAt(player_pos[i]);
+		player_img.at(i).at(0).mirrored(player_direction[i]).drawAt(player_pos[i][0]);
 	}
 }
--- Source/Game.hpp
+++ Source/Game.hpp
@@ -10,27 +10,29 @@ private:
 	//定数////////////////////////////////////////////////////////////
 	const static int player_sum = 2;
 	const static int player_min_y = 700;
-	const Vec2 go_left = {-1.0,0.0};
-	const Vec2 go_right = {1.0,0.0};
+	const static int player_status_sum = 10;
 	//画像////////////////////////////////////////////////////////////
 	const Texture background_img{ U"../images/background.png"};
 	std::vector<std::vector<Texture>> player_img;
 	std::vector<bool> player_flag;
 	//変数////////////////////////////////////////////////////////////
-	//ステータス(0:待機中,1:左移動,2:右移動,3ジャンプ,4:スタン,5:弱,6:狂,7:特殊,8～:予備)
+	//ステータス(0:待機中,1:左移動,2:右移動,4ジャンプ,8:スタン,16:弱,32:狂,64:特殊,128～:予備)
 	int player_status[player_sum] = {0};
 	//HP
 	int player_hp[player_sum] = {100};
 	//AP
 	int player_ap[player_sum] = { 10 };
-	Vec2 player_pos[player_sum] = { {100,player_min_y},{1200,player_min_y} };
+	Vec2 player_pos[player_sum][2];
 	//移動速度
-	double player_speed[player_sum] = {10.0};
-	//Playerに関する時間
-	int player_timer[player_sum] = {0};
+	double player_speed[player_sum] = {50.0};
+	//Playerに関する時間(0:左右移動,1:予備,2:ジャンプ)
+	int player_timer[player_sum][player_status_sum] = {0};
+	//Playerの向き(true:右,false:左)
+	bool player_direction[player_sum] = {false};
 	//時間
 	int internal_timer = 0;
 	//内部関数////////////////////////////////////////////////////////
+	//1:上,2:左,4:下,8:右
 	int getkey();
 public:
 
