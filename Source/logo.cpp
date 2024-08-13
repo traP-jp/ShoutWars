bf3f3c65dcb35b4b56aa2d85b8b1b92abf13e67a
UnABC <75528021+UnABC@users.noreply.github.com>
2024-08-14 02:32:56 +0900

    logo画面の追加

---
+++ Source/logo.cpp
@@ -0,0 +1,29 @@
+﻿#include "logo.hpp"
+
+logo::logo(const InitData& init) : IScene(init)
+{
+}
+
+void logo::update()
+{
+	if (first_time) {
+		logo_timer = Time::GetMillisec();
+		first_time = false;
+	}
+	if (Time::GetMillisec()- logo_timer > 1500)changeScene(State::Title);
+}
+
+void logo::draw() const
+{
+	fade_img.draw(0, 0);
+}
+
+void logo::drawFadeIn(double t) const
+{
+	fade_img.draw(0, 0, ColorF{ 1.0, t });
+}
+
+void logo::drawFadeOut(double t) const
+{
+	fade_img.draw(0, 0, ColorF{ 1.0, 1.0 - t });
+}
