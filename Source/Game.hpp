8678d8c9caa1f1227bfbbe6282e39bf367af2d7b
UnABC <75528021+UnABC@users.noreply.github.com>
2024-08-29 05:28:38 +0900

    必殺技のアニメーションとAPの手前まで

--- Source/Game.hpp
+++ Source/Game.hpp
@@ -8,18 +8,21 @@
 struct Player {
 	//各種変数
 	Vec2 pos[2];
-	//ステータス(0:待機中,1:左移動,2:右移動,4ジャンプ,8:スタン,16:弱,32:狂,64:特殊,128～:予備)
+	//ステータス(0:待機中,1:左移動,2:右移動,4ジャンプ,8:スタン,16:弱,32:狂,64:必殺,128～:予備)
 	int status = 0;
 	//HP(0:実質HP(確定),1:表示HP(未確定),2:表示HP(確定))
 	int hp[3] = { 1000,1000,1000 };
 	int ap = 0;
 	double speed = 50.0;
-	//Playerに関する時間(0:左右移動,1:予備,2:ジャンプ,3:スタン,4:弱,5:狂)
+	//Playerに関する時間(0:左右移動,1:予備,2:ジャンプ,3:スタン,4:弱,5:狂,6:必殺)
 	int timer[10];
 	//Playerの向き(true:右,false:左)
 	bool direction = false;
-	//通信が必要なイベント,[0]:イベント(0:なし,1:弱,2:狂,4:),[1]:時間
+	//通信が必要なイベント,[0]:イベント(0:なし,1:弱,2:狂,4:必殺),[1]:時間
 	int event[2];
+	//必殺技が使えるか
+	//XXX:debug用
+	bool special_attack = true;
 
 	int img_number = 0;
 	int img_status = 0;
@@ -37,12 +40,16 @@ private:
 	const static int player_status_sum = 10;
 	const static int player_max_hp = 1000;
 	//画像////////////////////////////////////////////////////////////
-	const Texture background_img{ U"../images/game/background.png"};
-	const Texture HP_bar_flame_img{ U"../images/game/HP_bar_flame.png" };
-	const Texture HP_bar_gray_img{ U"../images/game/HP_bar_gray.png" };
-	const Texture HP_bar_red_img{ U"../images/game/HP_bar_red.png" };
-	const Texture HP_bar_blue_img{ U"../images/game/HP_bar_blue.png" };
+	const Texture background_img{ U"../images/game/system/background.png"};
+	const Texture HP_bar_flame_img{ U"../images/game/system/HP_bar_flame.png" };
+	const Texture HP_bar_gray_img{ U"../images/game/system/HP_bar_gray.png" };
+	const Texture HP_bar_red_img{ U"../images/game/system/HP_bar_red.png" };
+	const Texture HP_bar_blue_img{ U"../images/game/system/HP_bar_blue.png" };
+	const Texture AP_bar_empty_img{ U"../images/game/system/AP_bar_empty.png" };
+	const Texture AP_bar_max_img{ U"../images/game/system/AP_bar_max.png" };
+	const Texture AP_bar_img{ U"../images/game/system/AP_bar.png" };
 	std::vector<std::vector<Texture>> player_img;
+	std::vector<Texture> fire_img;
 	//音楽////////////////////////////////////////////////////////////
 	const Audio bgm{ U"../audioes/Es-Boss3_loop.ogg" , Arg::loopBegin = 28.848843537415s};
 
@@ -63,8 +70,10 @@ private:
 	int getkey();
 	void draw_player() const;
 	void draw_HP_bar() const;
+	void draw_AP_bar() const;
 	void update_player();
 	void update_player_animation();
+	void update_AP_bar();
 	void init_connection();
 	void synchronizate_data();
 	int voice_command();
