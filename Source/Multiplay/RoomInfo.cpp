3a75435555cb62c1dc0b0899058070c6867149b2
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2024-08-30 03:45:01 +0900

    何もしない同期ができるように

--- Source/Multiplay/RoomInfo.cpp
+++ Source/Multiplay/RoomInfo.cpp
@@ -2,8 +2,6 @@
 
 RoomInfo::RoomInfo() = default;
 
-// メンバの初期化は初期化子リストを使ってください。
-// 複雑な初期化が必要な場合は protected static 関数を作って噛ませてください。
 RoomInfo::RoomInfo(const JSON& json) {}
 
 JSON RoomInfo::toJSON() const {
