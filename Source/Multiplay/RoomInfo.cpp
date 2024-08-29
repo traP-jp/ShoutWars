183b1e2dc2a887535548fce91bf58135683edecd
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2024-08-29 19:39:43 +0900

    SyncClient のベースを作成

---
+++ Source/Multiplay/RoomInfo.cpp
@@ -0,0 +1,11 @@
+﻿# include "RoomInfo.hpp"
+
+RoomInfo::RoomInfo() = default;
+
+// メンバの初期化は初期化子リストを使ってください。
+// 複雑な初期化が必要な場合は protected static 関数を作って噛ませてください。
+RoomInfo::RoomInfo(const JSON& json) {}
+
+JSON RoomInfo::toJSON() const {
+	return {};
+}
---
+++ Source/Multiplay/SyncClient.cpp
@@ -0,0 +1,41 @@
+﻿# include "SyncClient.hpp"
+
+using namespace std;
+
+AsyncTask<SyncClient> SyncClient::createRoom(shared_ptr<APIClient> api, const String& userName, uint32 size) {
+	return Async([=] {
+		const JSON response = api->send(APIClient::HTTPMethod::POST, U"/room/create", {
+			{ U"version", api->version },
+			{ U"user", { { U"name", userName } } },
+			{ U"size", size },
+		}).get();
+		return SyncClient{
+			api,
+			*UUIDValue::Parse(response[U"session_id"].getString()),
+			*UUIDValue::Parse(response[U"id"].getString()),
+			*UUIDValue::Parse(response[U"user_id"].getString()),
+			response[U"name"].getString(),
+		};
+	});
+}
+
+AsyncTask<SyncClient> SyncClient::joinRoom(shared_ptr<APIClient> api, const String& roomName, const String& userName) {
+	return Async([=] {
+		const JSON response = api->send(APIClient::HTTPMethod::POST, U"/room/join", {
+			{ U"version", api->version },
+			{ U"name", roomName },
+			{ U"user", { { U"name", userName } } },
+		}).get();
+		return SyncClient{
+			api,
+			*UUIDValue::Parse(response[U"session_id"].getString()),
+			*UUIDValue::Parse(response[U"id"].getString()),
+			*UUIDValue::Parse(response[U"user_id"].getString()),
+			userName,
+			RoomInfo{ response[U"room_info"] },
+		};
+	});
+}
+
+SyncClient::SyncClient(shared_ptr<APIClient> api, UUIDValue sessionId, UUIDValue roomId, UUIDValue userId, const String& roomName, const RoomInfo& roomInfo)
+	: api(api), sessionId(sessionId), roomId(roomId), userId(userId), roomName(roomName), roomInfo(roomInfo) {}
