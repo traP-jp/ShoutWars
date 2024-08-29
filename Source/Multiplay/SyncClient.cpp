d3b2a5c6c8a389dc9e6c0295c6dcc3f573c40dcc
TwoSquirrels <47352965+TwoSquirrels@users.noreply.github.com>
2024-08-30 03:45:01 +0900

    何もしない同期ができるように

--- Source/Multiplay/SyncClient.cpp
+++ Source/Multiplay/SyncClient.cpp
@@ -2,40 +2,108 @@
 
 using namespace std;
 
-AsyncTask<SyncClient> SyncClient::createRoom(shared_ptr<APIClient> api, const String& userName, uint32 size) {
+AsyncTask<shared_ptr<SyncClient>> SyncClient::createRoom(shared_ptr<APIClient> api, const String& userName, uint32 size) {
 	return Async([=] {
 		const JSON response = api->send(APIClient::HTTPMethod::POST, U"/room/create", {
 			{ U"version", api->version },
 			{ U"user", { { U"name", userName } } },
 			{ U"size", size },
 		}).get();
-		return SyncClient{
+		const auto client = make_shared<SyncClient>(
 			api,
 			*UUIDValue::Parse(response[U"session_id"].getString()),
 			*UUIDValue::Parse(response[U"id"].getString()),
 			*UUIDValue::Parse(response[U"user_id"].getString()),
-			response[U"name"].getString(),
-		};
+			response[U"name"].getString()
+		);
+		System::Sleep(Interval);
+		client->startSync();
+		client->finishSync();
+		return client;
 	});
 }
 
-AsyncTask<SyncClient> SyncClient::joinRoom(shared_ptr<APIClient> api, const String& roomName, const String& userName) {
+AsyncTask<shared_ptr<SyncClient>> SyncClient::joinRoom(shared_ptr<APIClient> api, const String& roomName, const String& userName) {
 	return Async([=] {
 		const JSON response = api->send(APIClient::HTTPMethod::POST, U"/room/join", {
 			{ U"version", api->version },
 			{ U"name", roomName },
 			{ U"user", { { U"name", userName } } },
 		}).get();
-		return SyncClient{
+		const JSON roomInfoJSON = response[U"room_info"];
+		const auto client = make_shared<SyncClient>(
 			api,
 			*UUIDValue::Parse(response[U"session_id"].getString()),
 			*UUIDValue::Parse(response[U"id"].getString()),
 			*UUIDValue::Parse(response[U"user_id"].getString()),
 			userName,
-			RoomInfo{ response[U"room_info"] },
-		};
+			roomInfoJSON.isObject() ? RoomInfo{ roomInfoJSON } : RoomInfo{}
+		);
+		System::Sleep(Interval);
+		client->startSync();
+		client->finishSync();
+		return client;
 	});
 }
 
 SyncClient::SyncClient(shared_ptr<APIClient> api, UUIDValue sessionId, UUIDValue roomId, UUIDValue userId, const String& roomName, const RoomInfo& roomInfo)
 	: api(api), sessionId(sessionId), roomId(roomId), userId(userId), roomName(roomName), roomInfo(roomInfo) {}
+
+const map<UUIDValue, SyncClient::User>& SyncClient::getUsers() const {
+	return users;
+}
+
+const SyncClient::User& SyncClient::getMe() const {
+	return users.at(userId);
+}
+
+bool SyncClient::isOwner() const {
+	if (users.empty()) return false;
+	return userId == users.begin()->first;
+}
+
+bool SyncClient::isSyncing() const {
+	return syncTask.isValid();
+}
+
+Optional<Error> SyncClient::update() {
+	try {
+		if (!syncTask.isValid() && timer.reachedZero()) startSync();
+		else if (syncTask.isReady()) finishSync();
+		return none;
+	}
+	catch (const Error& error) {
+		return error;
+	}
+}
+
+void SyncClient::startSync() {
+	if (syncTask.isValid()) throw Error(U"Already syncing");
+	if (!timer.reachedZero()) throw Error(U"Too early to sync");
+	syncTask = api->send(APIClient::HTTPMethod::POST, U"/room/sync", {
+		{ U"session_id", sessionId.str() },
+		{ U"room_info", roomInfo.toJSON() },
+		{ U"reports", U"[]"_json }, // TODO
+		{ U"actions", U"[]"_json }, // TODO
+	});
+}
+
+UUIDValue SyncClient::finishSync() {
+	if (!syncTask.isValid()) throw Error(U"Not syncing");
+	const JSON response = syncTask.get();
+	timer.restart();
+	for (const auto& [i, reportJSON] : response[U"reports"]) {
+		// TODO
+	}
+	for (const auto& [i, actionJSON] : response[U"actions"]) {
+		// TODO
+	}
+	for (const auto& [i, userJSON] : response[U"room_users"]) {
+		const User user{
+			.id = *UUIDValue::Parse(userJSON[U"id"].getString()),
+			.name = userJSON[U"name"].getString(),
+		};
+		users.emplace(user.id, user);
+	}
+	return *UUIDValue::Parse(response[U"id"].getString());
+}
--- Source/Multiplay/SyncClient.hpp
+++ Source/Multiplay/SyncClient.hpp
@@ -12,6 +12,8 @@ public:
 		const String name;
 	};
 
+	inline static const MillisecondsF Interval = 100ms;
+
 	/**
 	 * @brief エラーは APIClient::send も参照
 	 * @param api API クライアント
@@ -20,7 +22,7 @@ public:
 	 * @return 部屋を同期するためのクライアントの AsyncTask
 	 * @throw APIClient::HTTPError { 403: 部屋数の上限に達した }
 	 */
-	static [[nodiscard]] AsyncTask<SyncClient> createRoom(std::shared_ptr<APIClient> api, const String& userName, uint32 size = 2);
+	static [[nodiscard]] AsyncTask<std::shared_ptr<SyncClient>> createRoom(std::shared_ptr<APIClient> api, const String& userName, uint32 size = 2);
 
 	/**
 	 * @brief エラーは APIClient::send も参照
@@ -30,7 +32,7 @@ public:
 	 * @return 部屋を同期するためのクライアントの AsyncTask
 	 * @throw APIClient::HTTPError { 400: 部屋のバージョンが異なる, 403: 入室できない, 404: 部屋が存在しない }
 	 */
-	static [[nodiscard]] AsyncTask<SyncClient> joinRoom(std::shared_ptr<APIClient> api, const String& roomName, const String& userName);
+	static [[nodiscard]] AsyncTask<std::shared_ptr<SyncClient>> joinRoom(std::shared_ptr<APIClient> api, const String& roomName, const String& userName);
 
 	const std::shared_ptr<APIClient> api;
 
@@ -39,8 +41,35 @@ public:
 	const UUIDValue userId;
 	const String roomName; // 6 桁の数字
 
-protected:
-	RoomInfo roomInfo; // オーナー以外が更新したい時は同期でイベントを送る
+	RoomInfo roomInfo;
 
-	SyncClient(std::shared_ptr<APIClient> api, UUIDValue sessionId, UUIDValue roomId, UUIDValue userId, const String& roomName, const RoomInfo& roomInfo = RoomInfo{});
+	[[nodiscard]] SyncClient(std::shared_ptr<APIClient> api, UUIDValue sessionId, UUIDValue roomId, UUIDValue userId, const String& roomName, const RoomInfo& roomInfo = RoomInfo{});
+
+	[[nodiscard]] const std::map<UUIDValue, User>& getUsers() const;
+
+	[[nodiscard]] const User& getMe() const;
+
+	/**
+	 * @return 自分がオーナーかどうか
+	 */
+	[[nodiscard]] bool isOwner() const;
+
+	[[nodiscard]] bool isSyncing() const;
+
+	/**
+	 * @brief 同期の通信に関する処理をする時間ならする (毎フレーム呼び出す)
+	 * @return エラーがあればそれを返す。(ログなどに出力することを推奨) APIClient::send も参照
+	 * @retval none 問題無し
+	 * @retval APIClient::HTTPError { 401: セッションが無効, 403: 同期の条件を満たしていない, 404: 部屋が削除された, 429: 同期タイミングが早すぎる }
+	 */
+	Optional<Error> update();
+
+protected:
+	std::map<UUIDValue, User> users; // 一番 ID が若いのがオーナー
+	AsyncTask<JSON> syncTask;
+	Timer timer{ Interval, StartImmediately::Yes };
+
+	void startSync();
+
+	UUIDValue finishSync();
 };
