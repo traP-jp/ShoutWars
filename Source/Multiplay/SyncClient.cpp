375a1bcbb3bcf926c1d64013baf9b52af47623a4
TwoSquirrels <47352965+TwoSquirrels@users.noreply.github.com>
2024-08-31 15:15:25 +0900

    イベントを送受信できるように

--- Source/Multiplay/SyncClient.cpp
+++ Source/Multiplay/SyncClient.cpp
@@ -2,14 +2,32 @@
 
 using namespace std;
 
-AsyncTask<shared_ptr<SyncClient>> SyncClient::createRoom(shared_ptr<APIClient> api, const String& userName, uint32 size) {
+UUIDValue SyncClient::genUUIDv7() {
+	// ref: https://www.boost.org/doc/libs/1_86_0/boost/uuid/time_generator_v7.hpp
+
+	thread_local uint64 oldState = 0;
+	const uint64 nowUs = Time::GetMicrosecSinceEpoch();
+	uint64 state = ((nowUs / 1000) << 16) | ((nowUs % 1000) << 6);
+	if (state <= oldState && nowUs / 1000 >= (oldState >> 16)) state = oldState + 1;
+	oldState = state;
+
+	array<uint8, 16> uuid;
+	const uint64 random = Random(UINT64_MAX);
+	memcpy(uuid.data() + 8, &random, sizeof(uint64));
+	const uint64 timestamp_big_endian = byteswap(((state >> 16) << 16) | 0x7000 | ((state & 0xFFFF) >> 6));
+	memcpy(uuid.data(), &timestamp_big_endian, sizeof(uint64));
+	uuid[8] = 0x80 | (state & 0x3F);
+	return UUIDValue{ uuid };
+}
+
+AsyncTask<unique_ptr<SyncClient>> SyncClient::createRoom(shared_ptr<APIClient> api, const String& userName, uint32 size) {
 	return Async([=] {
 		const JSON response = api->send(APIClient::HTTPMethod::POST, U"/room/create", {
 			{ U"version", api->version },
 			{ U"user", { { U"name", userName } } },
 			{ U"size", size },
 		}).get();
-		const auto client = make_shared<SyncClient>(
+		auto client = make_unique<SyncClient>(
 			api,
 			*UUIDValue::Parse(response[U"session_id"].getString()),
 			*UUIDValue::Parse(response[U"id"].getString()),
@@ -23,7 +41,7 @@ AsyncTask<shared_ptr<SyncClient>> SyncClient::createRoom(shared_ptr<APIClient> a
 	});
 }
 
-AsyncTask<shared_ptr<SyncClient>> SyncClient::joinRoom(shared_ptr<APIClient> api, const String& roomName, const String& userName) {
+AsyncTask<unique_ptr<SyncClient>> SyncClient::joinRoom(shared_ptr<APIClient> api, const String& roomName, const String& userName) {
 	return Async([=] {
 		const JSON response = api->send(APIClient::HTTPMethod::POST, U"/room/join", {
 			{ U"version", api->version },
@@ -31,7 +49,7 @@ AsyncTask<shared_ptr<SyncClient>> SyncClient::joinRoom(shared_ptr<APIClient> api
 			{ U"user", { { U"name", userName } } },
 		}).get();
 		const JSON roomInfoJSON = response[U"room_info"];
-		const auto client = make_shared<SyncClient>(
+		auto client = make_unique<SyncClient>(
 			api,
 			*UUIDValue::Parse(response[U"session_id"].getString()),
 			*UUIDValue::Parse(response[U"id"].getString()),
@@ -48,6 +66,28 @@ AsyncTask<shared_ptr<SyncClient>> SyncClient::joinRoom(shared_ptr<APIClient> api
 
 SyncClient::SyncClient(shared_ptr<APIClient> api, UUIDValue sessionId, UUIDValue roomId, UUIDValue userId, const String& roomName, const RoomInfo& roomInfo)
 	: api(api), sessionId(sessionId), roomId(roomId), userId(userId), roomName(roomName), roomInfo(roomInfo) {}
+
+void SyncClient::sendReport(const String& type, const JSON& data) {
+	pendingReports << make_unique<Event>(genUUIDv7(), UUIDValue::Nil(), userId, type, data);
+}
+
+void SyncClient::sendAction(const String& type, const JSON& data) {
+	pendingActions << make_unique<Event>(genUUIDv7(), UUIDValue::Nil(), userId, type, data);
+}
+
+unique_ptr<SyncClient::Event> SyncClient::receiveReport() {
+	if (receivedReports.empty()) return nullptr;
+	auto event = move(receivedReports.front());
+	receivedReports.pop_front();
+	return event;
+}
+
+unique_ptr<SyncClient::Event> SyncClient::receiveAction() {
+	if (receivedActions.empty()) return nullptr;
+	auto event = move(receivedActions.front());
+	receivedActions.pop_front();
+	return event;
+}
 
 const map<UUIDValue, SyncClient::User>& SyncClient::getUsers() const {
 	return users;
@@ -92,11 +132,38 @@ Optional<SyncClient::StartInfo> SyncClient::receiveStart() {
 void SyncClient::startSync() {
 	if (syncTask.isValid()) throw Error(U"Already syncing");
 	if (!timer.reachedZero()) throw Error(U"Too early to sync");
-	syncTask = api->send(APIClient::HTTPMethod::POST, U"/room/sync", {
-		{ U"session_id", sessionId.str() },
-		{ U"room_info", roomInfo.toJSON() },
-		{ U"reports", U"[]"_json }, // TODO
-		{ U"actions", U"[]"_json }, // TODO
+	Array<JSON> reportsJSON, actionsJSON;
+	for (const auto& report : pendingReports) reportsJSON << JSON{
+		{ U"id", report->id.str() },
+		{ U"type", report->type },
+		{ U"event", report->data },
+	};
+	for (const auto& action : pendingActions) actionsJSON << JSON{
+		{ U"id", action->id.str() },
+		{ U"type", action->type },
+		{ U"event", action->data },
+	};
+	sentActions.swap(pendingActions);
+	sentReports.swap(pendingReports);
+	syncTask = Async([=] {
+		try {
+			const JSON response = api->send(APIClient::HTTPMethod::POST, U"/room/sync", {
+				{ U"session_id", sessionId.str() },
+				{ U"room_info", roomInfo.toJSON() },
+				{ U"reports", reportsJSON },
+				{ U"actions", actionsJSON },
+			}).get();
+			sentReports.clear();
+			sentActions.clear();
+			return response;
+		}
+		catch (const Error& error) {
+			for (auto& report : sentReports) pendingReports << move(report);
+			for (auto& action : sentActions) pendingActions << move(action);
+			sentReports.clear();
+			sentActions.clear();
+			throw error;
+		}
 	});
 }
 
@@ -104,11 +171,24 @@ UUIDValue SyncClient::finishSync() {
 	if (!syncTask.isValid()) throw Error(U"Not syncing");
 	const JSON response = syncTask.get();
 	timer.restart();
+	const UUIDValue syncId = *UUIDValue::Parse(response[U"id"].getString());
 	for (const auto& [i, reportJSON] : response[U"reports"]) {
-		// TODO
+		receivedReports << make_unique<Event>(
+			*UUIDValue::Parse(reportJSON[U"id"].getString()),
+			reportJSON.hasElement(U"sync_id") ? *UUIDValue::Parse(reportJSON[U"sync_id"].getString()) : syncId,
+			*UUIDValue::Parse(reportJSON[U"from"].getString()),
+			reportJSON[U"type"].getString(),
+			JSON::Parse(reportJSON[U"event"].formatMinimum()) // こうする以外の方法が見つからなかった
+		);
 	}
 	for (const auto& [i, actionJSON] : response[U"actions"]) {
-		// TODO
+		receivedActions << make_unique<Event>(
+			*UUIDValue::Parse(actionJSON[U"id"].getString()),
+			actionJSON.hasElement(U"sync_id") ? *UUIDValue::Parse(actionJSON[U"sync_id"].getString()) : syncId,
+			*UUIDValue::Parse(actionJSON[U"from"].getString()),
+			actionJSON[U"type"].getString(),
+			JSON::Parse(actionJSON[U"event"].formatMinimum()) // こうする以外の方法が見つからなかった
+		);
 	}
 	users.clear();
 	for (const auto& [i, userJSON] : response[U"room_users"]) {
@@ -118,5 +198,5 @@ UUIDValue SyncClient::finishSync() {
 		};
 		users.emplace(user.id, user);
 	}
-	return *UUIDValue::Parse(response[U"id"].getString());
+	return syncId;
 }
--- Source/Multiplay/SyncClient.hpp
+++ Source/Multiplay/SyncClient.hpp
@@ -7,6 +7,14 @@
 
 class SyncClient {
 public:
+	struct Event {
+		const UUIDValue id;
+		const UUIDValue syncId;
+		const UUIDValue from;
+		const String type;
+		const JSON data;
+	};
+
 	struct User {
 		const UUIDValue id;
 		const String name;
@@ -16,6 +24,8 @@ public:
 
 	inline static const MillisecondsF Interval = 100ms;
 
+	static [[nodiscard]] UUIDValue genUUIDv7();
+
 	/**
 	 * @brief エラーは APIClient::send も参照
 	 * @param api API クライアント
@@ -24,7 +34,7 @@ public:
 	 * @return 部屋を同期するためのクライアントの AsyncTask
 	 * @throw APIClient::HTTPError { 403: 部屋数の上限に達した }
 	 */
-	static [[nodiscard]] AsyncTask<std::shared_ptr<SyncClient>> createRoom(std::shared_ptr<APIClient> api, const String& userName, uint32 size = 2);
+	static [[nodiscard]] AsyncTask<std::unique_ptr<SyncClient>> createRoom(std::shared_ptr<APIClient> api, const String& userName, uint32 size = 2);
 
 	/**
 	 * @brief エラーは APIClient::send も参照
@@ -34,7 +44,7 @@ public:
 	 * @return 部屋を同期するためのクライアントの AsyncTask
 	 * @throw APIClient::HTTPError { 400: 部屋のバージョンが異なる, 403: 入室できない, 404: 部屋が存在しない }
 	 */
-	static [[nodiscard]] AsyncTask<std::shared_ptr<SyncClient>> joinRoom(std::shared_ptr<APIClient> api, const String& roomName, const String& userName);
+	static [[nodiscard]] AsyncTask<std::unique_ptr<SyncClient>> joinRoom(std::shared_ptr<APIClient> api, const String& roomName, const String& userName);
 
 	const std::shared_ptr<APIClient> api;
 
@@ -42,10 +52,38 @@ public:
 	const UUIDValue roomId;
 	const UUIDValue userId;
 	const String roomName; // 6 桁の数字
-
+	
 	RoomInfo roomInfo;
 
 	[[nodiscard]] SyncClient(std::shared_ptr<APIClient> api, UUIDValue sessionId, UUIDValue roomId, UUIDValue userId, const String& roomName, const RoomInfo& roomInfo = RoomInfo{});
+
+	/**
+	 * @brief 報告イベントの送信を予約する (自分には送信されない)
+	 * @param type イベントの種類
+	 * @param data イベントのデータ
+	 */
+	void sendReport(const String& type, const JSON& data);
+
+	/**
+	 * @brief 確認イベントの送信を予約する (自分にも送信される)
+	 * @param type イベントの種類
+	 * @param data イベントのデータ
+	 */
+	void sendAction(const String& type, const JSON& data);
+
+	/**
+	 * @brief 報告イベントを受信する (毎フレーム呼び出す)
+	 * @return 報告イベントがあればその情報を返す
+	 * @retval nullptr 全ての報告イベントを受信済み
+	 */
+	[[nodiscard]] std::unique_ptr<Event> receiveReport();
+
+	/**
+	 * @brief 確認イベントを受信する (毎フレーム呼び出す)
+	 * @return 確認イベントがあればその情報を返す
+	 * @retval nullptr 全ての確認イベントを受信済み
+	 */
+	[[nodiscard]] std::unique_ptr<Event> receiveAction();
 
 	[[nodiscard]] const std::map<UUIDValue, User>& getUsers() const;
 
@@ -81,10 +119,13 @@ public:
 	[[nodiscard]] Optional<StartInfo> receiveStart();
 
 protected:
+	Array<std::unique_ptr<Event>> pendingReports, pendingActions;
+	Array<std::unique_ptr<Event>> sentReports, sentActions; // 送信に失敗する可能性があるので保持
+	Array<std::unique_ptr<Event>> receivedReports, receivedActions;
 	std::map<UUIDValue, User> users; // 一番 ID が若いのがオーナー
 	AsyncTask<JSON> syncTask;
-	AsyncTask<JSON> startTask;
 	Timer timer{ Interval, StartImmediately::Yes };
+	AsyncTask<JSON> startTask;
 
 	void startSync();
 
